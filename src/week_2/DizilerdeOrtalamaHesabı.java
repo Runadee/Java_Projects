e482b7815d6cf9500edf54a7ee201af0fe10c993
Edanur <edanurgurgen94@gmail.com>
2024-02-27 00:08:47 +0300

    Array avarage calculated

---
+++ src/week_2/DizilerdeOrtalamaHesabı.java
@@ -0,0 +1,23 @@
+package week_2;
+
+import java.util.Scanner;
+
+public class DizilerdeOrtalamaHesabı {
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int toplam = 0;
+
+        int[] dizi = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Dizinin " + (i + 1) + ". elemanını giriniz: ");
+            dizi[i] = scan.nextInt();
+            toplam += dizi[i];
+        }
+        System.out.println("Dizinin ortalaması " + (toplam / dizi.length));
+    }
+}
+
