79250639112c0b55bbf0665eca3d5782c592fddc
Edanur <edanurgurgen94@gmail.com>
2024-02-27 16:37:46 +0300

    Arrays Frekans created

---
+++ src/week_2/ArraysFrekans.java
@@ -0,0 +1,31 @@
+package week_2;
+
+public class ArraysFrekans {
+
+
+    public static void main(String[] args) {
+        int[] dizi = {10, 20, 20, 10, 10, 20, 5, 20};
+        int n = dizi.length;
+        boolean[] ziyaretEdildi = new boolean[n];
+
+
+        for (int i = 0; i < n; i++) {
+
+            if (ziyaretEdildi[i])
+                continue;
+
+
+            int frekans = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (dizi[i] == dizi[j]) {
+                    frekans++;
+                    ziyaretEdildi[j] = true;
+                }
+            }
+
+            System.out.println(dizi[i] + " sayısı " + frekans + " kere tekrar edildi.");
+        }
+    }
+}
+
+
