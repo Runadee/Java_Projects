a74586150103a3360490df00557a229bd47e67ef
Edanur <edanurgurgen94@gmail.com>
2024-02-28 14:28:17 +0300

    added new conditions to NumberGuessGame

--- src/week_2/NumberGuessingGame.java
+++ src/week_2/NumberGuessingGame.java
@@ -1,17 +1,20 @@
 package week_2;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuessingGame {
     public static void main(String[] args) {
+
         Random randomObject = new Random();
         int number = randomObject.nextInt(100);
-        // int number = (int) (Math.random() * 100);
 
         Scanner input = new Scanner(System.in);
         int right = 0;
         int[] wrong = new int[5];
+        boolean isWin = false;
+        boolean isWrong = false;
 
         while (right < 5) {
             System.out.println("Please enter your guess number : ");
@@ -19,10 +22,19 @@ public class NumberGuessingGame {
 
             if (selected < 0 || selected > 99) {
                 System.out.println("Invalid Value! Please enter a value between 0 and 100.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("You entered many times incorrectly , remaining right: " + (5 - right));
+
+                } else {
+                    isWrong = true;
+                    System.out.println("The next time you enter incorrectly, your right will be decremented.");
+                }
                 continue;
             }
             if (selected == number) {
                 System.out.println("Congrats ! ");
+                isWin = true;
                 break;
             } else {
                 System.out.println("Sorry ! You didn't guess.");
@@ -35,6 +47,10 @@ public class NumberGuessingGame {
                 wrong[right++] = selected;
                 System.out.println("Your remaining right : " + (5 - right));
             }
+        }
+        if (!isWin) {
+            System.out.println("You failed !");
+            System.out.println("Your guesses : " + Arrays.toString(wrong));
         }
         System.out.println("The secret number is : " + number);
     }
